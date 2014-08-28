bc06fcb8ead9c133c92c95cec8d0149aefa30fb6
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-08-28 18:01:25 +0900

    Implement MWS::QueryString.

--- lib/mws/query_string.rb
+++ lib/mws/query_string.rb
@@ -4,5 +4,47 @@ require "mws/query_string/uri_encoder"
 
 module MWS
   class QueryString < ::String
+    DEFAULT_METHOD = "POST"
+    DEFAULT_PATH   = "/"
+    DEFAULT_PARAMS = {
+      "SignatureMethod" => "HmacSHA256",
+      "SignatureVersion" => "2"
+    }
+
+    def initialize(args)
+      @key    = args[:key]
+      @method = args[:method] || DEFAULT_METHOD
+      @host   = args[:host]
+      @path   = args[:path] || DEFAULT_PATH
+      @params = DEFAULT_PARAMS.merge(args[:params])
+
+      @params["Timestamp"] = timestamp_string
+      @params["Signature"] = signature_string # This substitution must be final substitution
+
+      super(query_string)
+    end
+
+    private
+
+    def query_string
+      encoder = MWS::QueryString::UriEncoder.new
+      sorted_params.each.map{|k, v| [k, encoder.encode(v)].join("=") }.join("&")
+    end
+
+    def timestamp_string
+      (@params["Timestamp"].is_a?(Time) ? @params["Timestamp"] : Time.now).iso8601
+    end
+
+    def signature_string
+      MWS::QueryString::Signature.new(request_string, @key)
+    end
+
+    def request_string
+      MWS::QueryString::RequestString.new(@method, @host, @path, @params)
+    end
+
+    def sorted_params
+      @params.sort_by{|k, _v| k }
+    end
   end
 end
--- spec/mws/query_string_spec.rb
+++ spec/mws/query_string_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe MWS::QueryString do
+  let(:query_string){ MWS::QueryString.new(valid_args) }
+  let(:valid_args){
+    {
+      key: "ThisIsSigningKey",
+      host: "mws.amazonservice.com",
+      params: {
+        "Action" => "SubmitFeed",
+        "Timestamp" => Time.new(2009, 8, 20, 1, 10, 27, 607)
+      }
+    }
+  }
+
+  describe "initialize" do
+    subject{ query_string }
+    it{ is_expected.to be_a String }
+    it("should be formated with query style"){ is_expected.to match /\A([^=&]+=[^=&]+)(&[^=&]+=[^=&]+)*\Z/ }
+    it("should be URI encoded"){ is_expected.not_to match /\A[A-Za-z0-9\-\_\.\~]*\Z/ }
+    it("should have timestamp in ISO8601 format"){ is_expected.to match /\d{4}-\d{2}-\d{2}T\d{2}%3A\d{2}%3A\d{2}/ }
+  end
 end
