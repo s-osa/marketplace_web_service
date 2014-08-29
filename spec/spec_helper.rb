7f643827e37296be60fae92df4f93b0ceb389c54
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-08-29 11:04:32 +0900

    Enable to require implicitly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ $: << project_root
 
 Dir[File.join(File.dirname(__FILE__), "support", "*")].each {|f| require f }
 
-require 'lib/mws'
+require 'lib/marketplace_web_service'
 
 require 'coveralls'
 Coveralls.wear!
