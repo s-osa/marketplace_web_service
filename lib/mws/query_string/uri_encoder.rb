82f5fdb4c32957e566ee35cd942e47296362308b
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-09-23 09:27:23 +0900

    Use uri_encoding gem to encode string.

--- lib/mws/query_string/uri_encoder.rb
+++ lib/mws/query_string/uri_encoder.rb
@@ -1,16 +1,10 @@
+require "uri_encoding"
+
 module MWS
   class QueryString < ::String
     class UriEncoder
-      NO_ENCODE_CHARACTERS = /[A-Za-z0-9\-\_\.\~]/
-
       def encode(string)
-        string.to_s.each_char.map{|char| char.match(NO_ENCODE_CHARACTERS) ? char : escape_char(char) }.join("")
-      end
-
-      private
-
-      def escape_char(char)
-        char.each_byte.map{|byte| "%#{byte.to_s(16).upcase}" }.join("")
+        UriEncoding.encode(string, UriEncoding::UnreservedCharacters)
       end
     end
   end
